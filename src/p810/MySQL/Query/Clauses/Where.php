1986c9329c77ff244f531d69923ebbd0c0f369b9
Payton <hellopayton@gmail.com>
2015-06-30 23:46:35 -0500

    Simplifies API for where clauses, adds the option for alternate operators

--- src/p810/MySQL/Query/Clauses/Where.php
+++ src/p810/MySQL/Query/Clauses/Where.php
@@ -3,15 +3,58 @@
 trait Where
 {
   /**
-   * Appends a simple where clause, asserting that a single column name is equal to the $value.
+   * Counts how many times this clause has been called.
+   *
+   * @access protected
+   * @var int
+   */
+  protected $whereIterations;
+
+
+  /**
+   * Appends a where clause.
+   *
+   * This method accepts variadic input, and retrieves arguments through func_get_args().
    *
    * @param string $column The name of the column to look up.
+   * @param string $operator (Optional) The operator to use.
    * @param mixed $value The value that the column should equal.
    * @return parent
    */
-  public function whereEquals($column, $value)
+  public function where()
   {
-    $this->statement[] = "WHERE `" . $column . "` = '" . $value . "'";
+    $arguments = func_get_args();
+
+    $clause = array();
+
+    if($this->whereIterations > 0) {
+      $clause[] = "AND";
+    } else {
+      $clause[] = "WHERE";
+    }
+
+    switch(count($arguments))
+    {
+      case 2:
+        $clause[] = "`" . $arguments[0] . "` = '" . $arguments[1] . "'";
+      break;
+
+      case 3:
+        if(!in_array($arguments[1], $this->operators)) {
+          throw new InvalidArgumentException;
+        }
+
+        $clause[] = "`" . $arguments[0] . '` ' . $arguments[1] . " '" . $arguments[2] . "'";
+      break;
+
+      default:
+        throw new InvalidArgumentException;
+      break;
+    }
+
+    $this->statement[] = implode(' ', $clause);
+
+    $this->whereIterations++;
 
     return $this;
   }
