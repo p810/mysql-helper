93988c1ddc0d242d907352df98f7f3756bf37e4e
Payton <hellopayton@gmail.com>
2015-07-01 17:54:26 -0500

    Adds the Limit trait

---
+++ src/p810/MySQL/Query/Clauses/Limit.php
@@ -0,0 +1,17 @@
+<?php namespace p810\MySQL\Query\Clauses;
+
+trait Limit
+{
+  /**
+   * Adds a LIMIT clause to the statement.
+   *
+   * @param int $total The total number of records to pull.
+   * @return self
+   */
+  public function limit($total)
+  {
+    $this->statement[] = "LIMIT " . $total;
+
+    return $this;
+  }
+}
