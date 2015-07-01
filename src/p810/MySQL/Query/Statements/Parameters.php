d032e6808aa8f9c5bb91ada900c2c9f6303b6ade
Payton <hellopayton@gmail.com>
2015-07-01 00:33:16 -0500

    Adds Parameters trait to dynamically allow for prepared statements

---
+++ src/p810/MySQL/Query/Statements/Parameters.php
@@ -0,0 +1,26 @@
+<?php namespace p810\MySQL\Query\Statements;
+
+trait Parameters
+{
+  /**
+   * Stores a list of parameters to be bound to the query.
+   *
+   * @access protected
+   * @var array
+   */
+  protected $parameters = array();
+
+
+  /**
+   * Adds a parameter to the list and returns its placeholder value.
+   *
+   * @param mixed $value The parameter to add to the query.
+   * @return string
+   */
+  public function addParam($value)
+  {
+    $this->parameters[] = $value;
+
+    return '?';
+  }
+}
