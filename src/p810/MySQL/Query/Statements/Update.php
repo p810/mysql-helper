d474f7285f779f196295ff5558dad64adc4b3736
Payton <hellopayton@gmail.com>
2015-07-01 17:46:24 -0500

    Adds documentation to the Update class

--- src/p810/MySQL/Query/Statements/Update.php
+++ src/p810/MySQL/Query/Statements/Update.php
@@ -7,11 +7,22 @@ extends Statement
 {
   use Where;
 
+
+  /**
+   * {@inheritdoc}
+   */
   public function begin()
   {
     $this->statement[] = "UPDATE";
   }
 
+  
+  /**
+   * Sets the table for the statement.
+   *
+   * @param string $table The table to update.
+   * @return self
+   */
   public function setTable($table)
   {
     $this->statement[] = "`" . $table . "`";
@@ -19,6 +30,13 @@ extends Statement
     return $this;
   }
 
+  
+  /**
+   * Sets the columns to update and their values.
+   *
+   * @param array $values A dictionary of columns to values to update.
+   * @return self
+   */
   public function setValues($values)
   {
     $this->statement[] = "SET";
@@ -35,6 +53,12 @@ extends Statement
     return $this;
   }
 
+  
+  /**
+   * Returns the row count that was affected by the query.
+   *
+   * @return int
+   */
   public function handleResults()
   {
     return $this->result->rowCount();
