9805f1b809f57b08d22f90b58847ed6dc1433ba0
Payton Bice <payton@platypi.io>
2015-10-26 18:15:44 -0500

    Refactors the Where clause to support and/or operators, slight API changes

--- src/p810/MySQL/Query/Clauses/Where.php
+++ src/p810/MySQL/Query/Clauses/Where.php
@@ -2,38 +2,97 @@
 
 trait Where
 {
-  /**
-   * Counts how many times this clause has been called.
-   *
-   * @access protected
-   * @var int
-   */
-  protected $whereIterations;
+  function __call($method, $arguments)
+  {
+    if ($method == 'and') {
+      $result = call_user_func_array([$this, '_and'], $arguments);
+    } elseif ($method == 'or') {
+      $result = call_user_func_array([$this, '_or'], $arguments);
+    }
+
+    return $result;
+  }
 
 
   /**
    * Appends a where clause.
    *
-   * This method accepts variadic input, and retrieves arguments through func_get_args().
+   * @param string $column The name of the column to look up.
+   * @param string $operator (Optional) The operator to use.
+   * @param mixed $value The value that the column should be compared against.
+   * @return $this
+   */
+  public function where(...$arguments)
+  {
+    $clause = array();
+
+    $clause[] = "WHERE";
+
+    $this->buildQueryString($clause, ...$arguments);
+
+    $this->statement[] = implode(' ', $clause);
+
+    return $this;
+  }
+
+
+  /**
+   * Appends a combining "and" operator to a where clause.
    *
    * @param string $column The name of the column to look up.
    * @param string $operator (Optional) The operator to use.
-   * @param mixed $value The value that the column should equal.
-   * @return parent
+   * @param mixed $value The value that the column should be compared against.
+   * @return $this
    */
-  public function where()
+  protected function _and(...$arguments)
   {
-    $arguments = func_get_args();
-
     $clause = array();
 
-    if($this->whereIterations > 0) {
-      $clause[] = "AND";
-    } else {
-      $clause[] = "WHERE";
-    }
+    $clause[] = "AND";
 
-    switch(count($arguments))
+    $this->buildQueryString($clause, ...$arguments);
+
+    $this->statement[] = implode(' ', $clause);
+
+    return $this;
+  }
+
+
+  /**
+   * Appends a combining "or" operator to a where clause.
+   * 
+   * @param string $column The name of the column to look up.
+   * @param string $operator (Optional) The operator to use.
+   * @param mixed $value The value that the column should be compared against.
+   * @return $this
+   */
+  protected function _or(...$arguments)
+  {
+    $clause = array();
+
+    $clause[] = "OR";
+
+    $this->buildqueryString($clause, ...$arguments);
+
+    $this->statement[] = implode(' ', $clause);
+
+    return $this;
+  }
+
+
+  /**
+   * Takes the variadic list of arguments from Where::where(), Where::and(), and Where::or() and creates a valid
+   * SQL statement from it.
+   *
+   * The result is appended to the array passed in as the first argument as it is passed by reference.
+   *
+   * @param array $clause A list of "tokens" to be joined for the SQL statement.
+   * @param array $arguments Whichever list was passed in from the above three methods.
+   * @return void
+   */
+  private function buildQueryString(&$clause, ...$arguments)
+  {
+    switch (count($arguments))
     {
       case 2:
         $clause[] = "`" . $arguments[0] . "` = " . $this->addParam($arguments[1]);
@@ -51,11 +110,5 @@ trait Where
         throw new InvalidArgumentException;
       break;
     }
-
-    $this->statement[] = implode(' ', $clause);
-
-    $this->whereIterations++;
-
-    return $this;
   }
 }
