f6e0ac477d8f4f2feccc659ebc03edfc66f136f2
Payton Bice <payton@platypi.io>
2015-10-29 16:45:33 -0500

    Adds Model::where() to allow conditional selects from a model

--- src/p810/MySQL/Model/Model.php
+++ src/p810/MySQL/Model/Model.php
@@ -25,4 +25,38 @@ extends Table
 
         return new Row($this, $data);
     }
+
+
+    /**
+     * Creates a select statement for the table represented by the model and appends a where clause.
+     *
+     * The returned value is an instance of Select.
+     *
+     * @param $arguments array A variadic list of arguments.
+     * @return object
+     */
+    public function where(...$arguments)
+    {
+      $handler = function ($results) {
+        $results = $results->fetchAll(\PDO::FETCH_ASSOC);
+
+        if (count($results) > 1) {
+          $rows = array();
+
+          foreach ($results as $data) {
+            $rows[] = new Row($this, $data);
+          }
+
+          return $rows;
+        } elseif (count($results) === 1) {
+          return new Row($this, array_shift($results));
+        } elseif (count($results) === 0 || !$results) {
+          return false;
+        }
+      };
+
+      return $this->resource->select('*', $this->getTableName())
+              ->setResultHandler($handler)
+              ->where(...$arguments);
+    }
 }
