3da5354f4ee8db4de79d15e6405e607aaae13e84
Payton <hellopayton@gmail.com>
2016-02-19 14:16:05 -0600

    Makes Row a little less opinionated again

--- src/p810/MySQL/Model/Row.php
+++ src/p810/MySQL/Model/Row.php
@@ -2,6 +2,7 @@
 
 namespace p810\MySQL\Model;
 
+use \Exception;
 use p810\MySQL\Connection;
 use \Doctrine\Common\Inflector\Inflector;
 use p810\MySQL\Relationships\Relationship;
@@ -16,15 +17,6 @@ class Row
      * @var object
      */
     protected $resource;
-
-
-    /**
-     * The ID of the row.
-     *
-     * @access protected
-     * @var int
-     */
-    protected $id;
 
 
     /**
@@ -64,12 +56,21 @@ class Row
 
 
     /**
-     * The primary key of the table being represented by this row.
+     * The primary key or a unique identifier of the table being represented by this row.
      *
      * @access protected
      * @var string
      */
-    protected $primaryKey;
+    protected $uniqueKey;
+
+
+    /**
+     * The ID of the primary key, or the value of whichever column is used as the unique identifier for the table.
+     *
+     * @access protected
+     * @var string|int
+     */
+    protected $uniqueValue;
 
 
     /**
@@ -85,20 +86,55 @@ class Row
         $this->resource = $resource;
 
         if (is_object($table) && $table instanceof Model) {
-            $this->table = $this->model->getTableName();
+            $this->table = $table->getTableName();
 
-            $this->primaryKey = $this->model->getPrimaryKey();
+            $this->identifyBy($table->getPrimaryKey());
         } else {
             $this->table = TableHelper::getTableName($table);
 
-            $this->primaryKey = TableHelper::getPrimaryKey($table);
+            $primaryKey = TableHelper::getPrimaryKey($table);
+
+            // Attempt to find the primary key so that most tables won't require a manual call to Row::identifyBy()
+            if (array_key_exists($primaryKey, $data)) {
+                $this->identifyBy($primaryKey);
+            }
         }
 
         $this->data = $data;
+    }
 
-        if (array_key_exists($this->primaryKey, $data)) {
-            $this->id = $data[$this->primaryKey];
+
+    /**
+     * Sets the value for Row::$uniqueKey, and optionally its corresponding value in the database (Row::$uniqueValue).
+     *
+     * @param string $column The column to identify this row by.
+     * @param string $value The value of $column, pointing to the row to select in the database.
+     * @return self
+     */
+    public function identifyBy($column, $value = null)
+    {
+        $this->uniqueKey = $column;
+
+        if (is_null($value)) {
+            if (array_key_exists($this->uniqueKey, $this->data)) {
+                $this->uniqueValue = $this->data[$this->uniqueKey];
+            }
+        } else {
+            $this->uniqueValue = $value;
         }
+
+        return $this;
+    }
+
+
+    /**
+     * An alias for Row::identifyBy.
+     *
+     * @see Row::identifyBy()
+     */
+    public function setPrimaryKey($column, $value = null)
+    {
+        return $this->identifyBy($column, $value);
     }
 
 
@@ -111,7 +147,7 @@ class Row
      */
     public function set($key, $value, $commit = true)
     {
-        if (!array_key_exists($key, $this->data) || !isset($this->primaryKey)) {
+        if (!array_key_exists($key, $this->data) || !isset($this->uniqueKey)) {
             $commit = false;
         }
 
@@ -158,7 +194,11 @@ class Row
      */
     public function relationship($relationship, ...$arguments)
     {
-        $relationship = new Relationship($this->resource, $this, $relationship, $arguments, $this->id);
+        if (!isset($this->uniqueValue)) {
+            throw new \Exception('A relationship cannot be mapped to an unidentifiable row');
+        }
+
+        $relationship = new Relationship($this->resource, $this, $relationship, $arguments, $this->uniqueValue);
 
         $relationship->setLocalTable($this->table);
 
@@ -174,7 +214,7 @@ class Row
      */
     private function commit($data)
     {
-        $query = $this->resource->update($this->table, $data)->where($this->primaryKey, $this->id);
+        $query = $this->resource->update($this->table, $data)->where($this->uniqueKey, $this->uniqueValue);
 
         $result = $query->execute();
 
