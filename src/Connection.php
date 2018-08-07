12d8c985cb2f9e564bb77a65b179801b7570a011
Payton Bice <hellopayton@gmail.com>
2018-08-06 20:34:24 -0500

    Moved Connection class to new directory

---
+++ src/Connection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace p810\MySQL;
+
+class Connection {
+    /**
+     * Database object returned by PDO.
+     * @var PDO
+     */
+    protected $database;
+
+    function __construct(
+        string $username, string $password, string $database,
+        string $host = '127.0.0.1', ?array $options = null
+    ) {
+        $arguments = [sprintf('mysql:host=%s;dbname=%s', $host, $database), $username, $password];
+
+        if ($options) {
+            $arguments[] = $options;
+        }
+        
+        try {
+            $this->resource = new \PDO(...$arguments);
+        } catch (\PDOException $e) {
+            throw new ConnectionException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+
+    public function getResource(): \PDO {
+        return $this->database;
+    }
+}
