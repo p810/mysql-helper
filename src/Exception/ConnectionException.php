99fb16846e07647ca1bafa17b7d5cfa2221ad9df
Payton Bice <hellopayton@gmail.com>
2018-08-06 20:07:13 -0500

    MySQLConnectionException -> ConnectionException

---
+++ src/Exception/ConnectionException.php
@@ -0,0 +1,4 @@
+<?php
+
+namespace p810\MySQL\Exception;
+class ConnectionException extends \Exception {}
--- src/p810/MySQL/Exceptions/MySQLConnectionException.php
+++
@@ -1,5 +0,0 @@
-<?php namespace p810\MySQL\Exceptions;
-
-use \Exception;
-
-class MySQLConnectionException extends Exception {}
