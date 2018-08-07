cad9645045ba63662b27ae760788758d51194819
Payton Bice <hellopayton@gmail.com>
2018-08-07 17:17:18 -0500

    Adds From trait, refactors Select

--- src/Builder/Select.php
+++ src/Builder/Select.php
@@ -4,6 +4,7 @@ namespace p810\MySQL\Builder;
 
 class Select extends Builder {
     use \p810\MySQL\Query\Where;
+    use \p810\MySQL\Query\From;
 
     public function build(): string {
         $query = sprintf('SELECT %s FROM %s', $this->getColumns(), $this->getTable());
@@ -14,29 +15,5 @@ class Select extends Builder {
         }
 
         return $query;
-    }
-
-    public function getColumns(): string {
-        if (is_array($this->fragments['columns'])) {
-            return implode(', ', $this->fragments['columns']);
-        }
-
-        return $this->fragments['columns'];
-    }
-
-    public function setColumns($columns): self {
-        $this->fragments['columns'] = $columns;
-
-        return $this;
-    }
-
-    public function getTable(): string {
-        return $this->fragments['table'];
-    }
-
-    public function from(string $table): self {
-        $this->fragments['table'] = $table;
-
-        return $this;
     }
 }
---
+++ src/Query/From.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace p810\MySQL\Query;
+
+trait From {
+    public function getColumns(): string {
+        if (is_array($this->fragments['columns'])) {
+            return implode(', ', $this->fragments['columns']);
+        }
+
+        return $this->fragments['columns'];
+    }
+
+    public function setColumns($columns): self {
+        $this->fragments['columns'] = $columns;
+
+        return $this;
+    }
+
+    public function getTable(): string {
+        return $this->fragments['table'];
+    }
+
+    public function setTable(string $table): self {
+        $this->fragments['table'] = $table;
+
+        return $this;
+    }
+
+    /**
+     * An alias of From::setTable() for looks.
+     */
+    public function from(string $table): self {
+        return $this->setTable($table);
+    }
+}
