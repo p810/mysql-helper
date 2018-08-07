0d3965ef57d3cac052f28652c191ad75fbc9d2d8
Payton Bice <hellopayton@gmail.com>
2018-08-07 10:19:58 -0500

    Adds base Builder class

---
+++ src/Builder/Builder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace p810\MySQL\Builder;
+
+abstract class Builder {
+    /**
+     * An associative array mapping parts of a
+     * query string to their values.
+     * @var string[]
+     */
+    protected $fragments;
+
+    /**
+     * The Query class used to instantiate the Builder.
+     * @var p810\MySQL\Query
+     */
+    protected $query;
+
+    function __construct(Query $query) {
+        $this->query = $query;
+    }
+
+    /**
+     * Using the Query object passed into BuilderInterface::setQuery()
+     * the class that implements this interface should be able to construct
+     * a query string via this method.
+     */
+    abstract public function build(): string;
+}
