04e9d81872a54392cf5501a20f79d112d8338c7d
Payton Bice <hellopayton@gmail.com>
2018-08-07 10:36:06 -0500

    Initial test cases for Select

---
+++ test/SelectQueryBuilderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use p810\MySQL\Query;
+use p810\MySQL\Builder\Select;
+use p810\MySQL\Builder\Builder;
+use PHPUnit\Framework\TestCase;
+
+class SelectQueryBuilderTest extends TestCase {
+    public function testQueryReturnsBuilder(): Select {
+        $query = new Query;
+        $query = $query->select('*');
+
+        $this->assertInstanceOf(Select::class, $query);
+
+        return $query;
+    }
+
+    /**
+     * @depends testQueryReturnsBuilder
+     */
+    public function testQueryValuesAreCorrect(Select $query): Select {
+        $this->assertEquals('*', $query->getColumns());
+
+        $query->from('test_table');
+
+        $this->assertEquals('test_table', $query->getTable());
+
+        return $query;
+    }
+
+    /**
+     * @depends testQueryValuesAreCorrect
+     */
+    public function testQueryBuildsQueryString(Select $query) {
+        $this->assertEquals('SELECT * FROM test_table', $query->build());
+    }
+}
