ac8fa674bfc29f06eb4cbcd6ef3c1dc9d7877682
Payton Bice <hellopayton@gmail.com>
2018-08-09 15:49:06 -0500

    Sets default value for $message

--- src/Exception/QueryExecutionException.php
+++ src/Exception/QueryExecutionException.php
@@ -5,7 +5,7 @@ namespace p810\MySQL\Exception;
 use \Throwable;
 
 class QueryExecutionException extends \Exception {
-    function __construct(string $message, int $code = 0, ?Throwable $previous = null) {
+    function __construct(string $message = '', int $code = 0, ?Throwable $previous = null) {
         // If a PDOException was thrown, lets use its message since it
         // will be helpful for debugging why the query couldn't execute
         if ($previous && ($previous instanceof \PDOException)) {
