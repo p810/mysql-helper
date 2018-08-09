e197bc0dfb6867fda11da34f11e0fc7f27b58c16
Payton Bice <hellopayton@gmail.com>
2018-08-09 11:17:25 -0500

    Adds QueryExecutionException

---
+++ src/Exception/QueryExecutionException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace p810\MySQL\Exception;
+
+use \Throwable;
+
+class QueryExecutionException extends \Exception {
+    function __construct(string $message, int $code = 0, ?Throwable $previous = null) {
+        // If a PDOException was thrown, lets use its message since it
+        // will be helpful for debugging why the query couldn't execute
+        if ($previous && ($previous instanceof \PDOException)) {
+            $message = $previous->getMessage();
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+}
