89d3c7d33b9bce70221cc742fe673d94e895fe5b
Payton Bice <hellopayton@gmail.com>
2018-09-10 14:12:36 -0500

    Adds Values trait

---
+++ src/Query/Values.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace p810\MySQL\Query;
+
+trait Values {
+    public function setValues(...$arguments): self {
+        switch (count($arguments)) {
+            case 1:
+                if (! is_array($arguments)) {
+                    throw new \UnexpectedValueException('Update::set() requires either a list of column => value pairs, or two arguments');
+                }
+
+                $arguments = $arguments[0];
+
+                foreach ($arguments as $column => $value) {
+                    $this->bind($value);
+                    $arguments[$column] = '?';
+                }
+            break;
+
+            case 2:
+                [$column, $value] = $arguments;
+
+                $this->bind($value);
+
+                $arguments = ["$column" => '?'];
+            break;
+        }
+
+        if (isset($this->fragments['values'])) {
+            $this->fragments['values'] += $arguments;
+        } else {
+            $this->fragments['values'] = $arguments;
+        }
+
+        return $this;
+    }
+
+    public function set(...$arguments): self {
+        return $this->setValues(...$arguments);
+    }
+
+    public function getValues(): string {        
+        $string = '';
+        foreach ($this->fragments['values'] as $column => $questionMark) {
+            $string .= "$column = ?";
+
+            end($this->fragments['values']);
+            if (key($this->fragments['values']) !== $column) {
+                $string .= ', ';
+            }
+        }
+
+        return $string;
+    }
+}
