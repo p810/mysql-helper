877c510838a66d32252b45541fcdbd810b8bbde7
Payton Bice <hellopayton@gmail.com>
2018-09-11 19:15:09 -0500

    Programatically finds primary key, return Row from where()

--- src/Model.php
+++ src/Model.php
@@ -2,6 +2,7 @@
 
 namespace p810\MySQL;
 
+use p810\MySQL\Model\Row;
 use p810\MySQL\Builder\Select;
 
 abstract class Model {
@@ -18,25 +19,64 @@ abstract class Model {
      */
     protected $database;
 
+    /**
+     * The column set as the table's primary key.
+     * @var string
+     */
+    protected $primaryKey;
+
     function __construct(Connection $connection) {
         $this->database = $connection->getResource();
 
         if (! Query::isConnected()) {
             Query::setConnection($connection);
         }
+
+        if (! $this->primaryKey) {
+            $column = $this->queryForPrimaryKey();
+
+            if ($column) {
+                $this->primaryKey = $column;
+            }
+        }
+    }
+
+    protected function queryForPrimaryKey(): ?string {
+        $query = $this->database->query("SHOW KEYS FROM {$this->table} WHERE Key_name = 'primary'");
+
+        if ($query->execute() === false) {
+            return null;
+        }
+
+        $results = $query->fetchAll(\PDO::FETCH_ASSOC);
+
+        if (empty($results)) {
+            return null;
+        }
+
+        $data = $results[0];
+
+        return $data['Column_name'];
     }
 
     final public function getTable(): string {
         return $this->table;
     }
 
-    public function where(...$clauses): Select {
-        $query = Query::select();
+    final public function getPrimaryKey(): ?string {
+        return $this->primaryKey;
+    }
 
-        $query->from( $this->getTable() );
-        
-        $query->where(...$clauses);
+    public function where(...$clauses): ?Row {
+        $data = Query::select()
+            ->from( $this->getTable() )
+            ->where(...$clauses)
+            ->execute();
 
-        return $query;
+        if (! $data) {
+            return null;
+        }
+
+        return new Row($data, $this);
     }
 }
