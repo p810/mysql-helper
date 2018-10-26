1187106ada63139fe0510b01876592d933b2bbd2
Payton Bice <hellopayton@gmail.com>
2018-10-26 16:27:38 -0500

    Adds functions.php

---
+++ src/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace p810\MySQL;
+
+use p810\MySQL\Query;
+use p810\MySQL\Builder\Builder;
+
+function select($columns = '*'): Builder {
+    return Query::select($columns);
+}
+
+function insert(string $table): Builder {
+    return Query::insert($table);
+}
+
+function update(string $table): Builder {
+    return Query::update($table);
+}
+
+function delete(): Builder {
+    return Query::delete();
+}
