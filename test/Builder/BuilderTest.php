606aea0798a612e2eac48b6b12e61841e4b0f12f
Payton Bice <hellopayton@gmail.com>
2019-04-13 16:39:05 -0500

    Remove setUp(), no longer used

--- test/Builder/BuilderTest.php
+++ test/Builder/BuilderTest.php
@@ -9,48 +9,6 @@ use PHPUnit\Framework\TestCase;
 
 class BuilderTest extends TestCase
 {
-    /**
-     * @var \stdClass<\p810\MySQL\Builder\Builder>
-     */
-    protected $builder;
-
-    public function setUp()
-    {
-        $this->builder = new class extends Builder
-        {
-            protected $order = ['select', 'from', 'where', 'limit'];
-
-            /**
-             * @var string[]
-             */
-            protected $tokens;
-
-            public function select($columns): self
-            {
-                if (is_array($columns)) {
-                    $columns = implode(', ', $columns);
-                }
-
-                return $this->append(Token::SELECT, $columns);
-            }
-
-            public function from(string $table): self
-            {
-                return $this->append(Token::FROM, $table);
-            }
-
-            public function where(string $column, $value, string $operator = '='): self
-            {
-                return $this->append(Token::WHERE, $column, $operator, $value);
-            }
-
-            public function limit(int $limit): self
-            {
-                return $this->append(Token::LIMIT, $limit);
-            }
-        };
-    }
-
     public function test_builder_construction_order()
     {
         $query = new Select;
