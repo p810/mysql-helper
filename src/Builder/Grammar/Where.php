33e545a0a629c403142e84850ac622535977b8ef
Payton Bice <hellopayton@gmail.com>
2019-04-14 14:20:48 -0500

    Refactored Where trait

--- src/Builder/Grammar/Where.php
+++ src/Builder/Grammar/Where.php
@@ -6,58 +6,19 @@ use p810\MySQL\Query;
 use InvalidArgumentException;
 use p810\MySQL\Builder\Token;
 
-use function key;
-use function next;
-use function count;
-use function sprintf;
-use function is_array;
-use function array_map;
-use function array_reduce;
-
 trait Where
 {
     /**
-     * @var \p810\MySQL\Builder\Grammar\Clause[]
+     * @var \p810\MySQL\Builder\Grammar\Expression[]
      */
-    protected $clauses;
-
-    /**
-     * @var string|null
-     */
-    protected $nextLogicalOperator;
+    protected $wheres;
 
     /**
      * @param mixed $value
      */
     public function where(string $column, $value, string $operator = '=', string $logical = 'and'): self
     {
-        if ($this->nextLogicalOperator) {
-            $logical = $this->nextLogicalOperator;
-            $this->nextLogicalOperator = null;
-        }
-
-        $this->clauses[] = new Clause($column, $this->bind($value), $operator, $logical);
-
-        return $this->append(Token::WHERE, $this->getWhereClause());
-    }
-
-    public function and(): self
-    {
-        $this->nextLogicalOperator = 'and';
-
-        return $this;
-    }
-
-    public function or(): self
-    {
-        $this->nextLogicalOperator = 'or';
-
-        return $this;
-    }
-
-    public function between(): self
-    {
-        $this->nextLogicalOperator = 'between';
+        $this->wheres[] = new Expression($column, $this->bind($value), $operator, $logical);
 
         return $this;
     }
@@ -136,19 +97,8 @@ trait Where
         return $this->where($columnOrExpression, $value, 'not in', $logical);
     }
 
-    protected function getWhereClause(): string
+    protected function compileWhere(): string
     {
-        $total = count($this->clauses);
-
-        return array_reduce($this->clauses, function ($value, $clause) use ($total) {
-            $value .= $clause->compile();
-
-            if (key($this->clauses) < $total - 1) {
-                $next = next($this->clauses);
-                $value .= sprintf(' %s ', $next->logicalOperator);
-            }
-
-            return $value;
-        }, '');
+        return 'where ' . $this->compileExpressions($this->wheres);
     }
 }
