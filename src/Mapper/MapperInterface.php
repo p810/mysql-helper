a5c12e02ce209b282501abef42ebe7cc7e29578e
Payton Bice <hellopayton@gmail.com>
2019-04-21 19:30:47 -0500

    Adds new methods to MapperInterface updateById(), deleteById(), and create()

--- src/Mapper/MapperInterface.php
+++ src/Mapper/MapperInterface.php
@@ -23,10 +23,39 @@ interface MapperInterface
      * Returns a new instance of the entity this mapper represents, if the adapter has data where
      * the entity's unique identifier is equal to the given $id
      * 
-     * Some entities may not need this functionality and may ignore it
+     * Some mappers may not need this functionality and may ignore it
      * 
      * @param int $id
      * @return null|object
      */
     public function findById(int $id): ?object;
+
+    /**
+     * Updates the row with the given ID with the given entity for data
+     * 
+     * Some mappers may not need this functionality and may ignore it
+     * 
+     * @param int    $id     The ID of the record to update
+     * @param object $entity The entity from which data should be pulled
+     * @return bool
+     */
+    public function updateById(int $id, object $entity): bool;
+
+    /**
+     * Deletes the row with the given ID
+     * 
+     * Some mappers may not need this functionality and may ignore it
+     * 
+     * @param int $id The ID of the record to delete
+     * @return bool
+     */
+    public function deleteById(int $id): bool;
+
+    /**
+     * Inserts a new row with data from the given entity
+     * 
+     * @param object $entity The entity from which data should be pulled
+     * @return bool
+     */
+    public function create(object $entity): bool;
 }
