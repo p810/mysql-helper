49c6d53ee619a3bdc671b98daa9f4f3a04cb0a74
Payton Bice <hellopayton@gmail.com>
2019-04-24 17:43:12 -0500

    Initial commit of HelperFunctionsTest.php

---
+++ test/HelperFunctionsTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace p810\MySQL\Test;
+
+use PHPUnit\Framework\TestCase;
+
+use function p810\MySQL\spaces;
+use function p810\MySQL\commas;
+use function p810\MySQL\parentheses;
+
+class HelperFunctionsTest extends TestCase
+{
+    public function test_spaces()
+    {
+        $this->assertEquals('hello world', spaces(['hello', 'world']));
+    }
+
+    public function test_commas()
+    {
+        $this->assertEquals('hello, world', commas(['hello', 'world']));
+    }
+
+    public function test_parentheses_with_array()
+    {
+        $this->assertEquals('(hello, world)', parentheses(['hello', 'world']));
+    }
+
+    public function test_parentheses_with_string()
+    {
+        $this->assertEquals('(hello world)', parentheses('hello world'));
+    }
+}
